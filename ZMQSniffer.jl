ea51ed6f91406a2506fe7ea3169dea450d842a5e
Jay Kickliter <jay@kickliter.com>
2015-03-23 13:25:09 -0600

    Sniffer is working pretty well. Need to put ot rest

---
+++ ZMQSniffer.jl
@@ -0,0 +1,16 @@
+using Dates
+using ZMQ
+
+ctx = Context(1)
+s1  = Socket(ctx, PULL)
+
+connect( s1, "tcp://localhost:5555")
+ 
+while true
+    msg = recv( s1 )
+    print( unix2datetime(time()), "    " )
+    for i in 1:length( msg )
+        @printf( "%02x ", msg[i] )
+    end
+    println()
+end
