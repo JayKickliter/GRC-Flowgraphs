bc78befe648e1c633568887514c1f87a7c1256bd
Jay Kickliter <jay@kickliter.com>
2015-03-23 16:48:48 -0600

    moved to function

--- ZMQSniffer.jl
+++ ZMQSniffer.jl
@@ -1,16 +1,20 @@
 using Dates
 using ZMQ
 
-ctx = Context(1)
-s1  = Socket(ctx, PULL)
 
-connect( s1, "tcp://localhost:5555")
- 
-while true
-    msg = recv( s1 )
-    print( unix2datetime(time()), "    " )
-    for i in 1:length( msg )
-        @printf( "%02x ", msg[i] )
+function sniff()
+    ctx = Context(1)
+    s1  = Socket(ctx, PULL)
+    connect( s1, "tcp://localhost:5555")
+    
+    while true
+        msg = recv( s1 )
+        print( unix2datetime(time()), "    " )
+        for i in 1:length( msg )
+            @printf( "%02x ", msg[i] )
+        end
+        println()
     end
-    println()
 end
+
+sniff()
